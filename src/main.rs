14ddde53ef1b49ecd0399c5bd0d0cac1c9d54c3e
Young Seo Kim <youngseokim7@gmail.com>
2025-11-21 21:03:09 +0900

    Fix bug with sensitive/insensitive case swap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rustgrep::search;
+use rustgrep::{search, search_case_insensitive};
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -20,15 +20,23 @@ fn main() {
 
 fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
+
     Ok(())
 }
 
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -38,8 +46,15 @@ impl Config {
         } else if args.len() > 3 {
             return Err("too many arguments. Need exactly 2!");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
