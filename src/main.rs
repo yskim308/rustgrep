7e9c089bf98f5a8409ca9092fddfb812dd2adb3f
Young Seo Kim <youngseokim7@gmail.com>
2025-11-21 12:06:09 +0900

    read file from root

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
-
     println!("searching for {query} in {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("{file_path} not found");
+    println!("in text:\n {contents}");
 }
