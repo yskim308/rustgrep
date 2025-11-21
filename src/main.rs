f9504591973a05d8239d2d9514087de47f54f218
Young Seo Kim <youngseokim7@gmail.com>
2025-11-21 20:17:51 +0900

    config abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("searching for {query} in {file_path}");
 
-    let contents = fs::read_to_string(file_path).expect("{file_path} not found");
-    println!("in text:\n {contents}");
+    let contents = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("too few arguments. Need exactly 2!");
+        } else if args.len() > 3 {
+            return Err("too many arguments. Need exactly 2!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
